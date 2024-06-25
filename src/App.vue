4eacda197bade0d52319fd317d307dfed66b6af2
Chang-Shih-Yung <henry1010921@gmail.com>
2024-06-25 13:12:37 +0800

    引入引入element UI

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+// import { useRoute } from 'vue-router'
+//獲取當前路由參數（內部細項目）
+// const route = useRoute()
+//獲取路由對象
+const router = useRouter()
+
+const home = () => {
+  router.push('/home')
+}
+
+const list = () => {
+  router.push('/list')
+}
+</script>
 
 <template>
   <div>我是App</div>
+  <TestDemo></TestDemo>
+  <el-button @click="home">home</el-button>
+  <el-button @click="list">list</el-button>
 </template>
 
 <style scoped></style>
