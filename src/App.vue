f86609366d14f29934042063fa58bfc4fe036aec
chang-shih-yung <henry1010921@gmail.com>
2024-11-05 14:01:14 +0800

    新增登錄註冊靜態畫面

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <script setup>
 import { User, Lock } from '@element-plus/icons-vue'
 import { ref } from 'vue'
+
 // isRegister的true或false，來切換註冊或登錄表單
 const isRegister = ref(true)
+
+/*用戶註冊*/
+// 整個用於提交el-form的對象：裡面有username、password、repassword
+const formModel = ref({
+  username: '',
+  password: '',
+  repassword: ''
+})
+
+// el-form表單驗證規則
+const rules = ref({
+  username: [{ required: true, message: '請輸入用戶名', trigger: 'blur' }],
+  password: [{ required: true, message: '請輸入密碼', trigger: 'blur' }],
+  repassword: [{ required: true, message: '請再次輸入密碼', trigger: 'blur' }]
+})
 </script>
 
 <template>
@@ -13,39 +29,64 @@ const isRegister = ref(true)
     <el-col :span="6" :offset="3" class="form">
       <!-- el-form是表單組件 -->
       <!-- isRegister 為true，看到註冊相關表單  -->
-      <el-form ref="form" size="large" autocomplete="off" v-if="isRegister">
+      <el-form
+        :model="formModel"
+        :rules="rules"
+        ref="form"
+        size="large"
+        autocomplete="off"
+        v-if="isRegister"
+      >
         <!-- 表單的一行 -->
         <el-form-item>
           <h1>用戶註冊</h1>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="username">
           <!-- el-input輸入框 -->
-          <el-input :prefix-icon="User" placeholder="請輸入用戶名"></el-input>
+          <el-input
+            v-model="formModel.username"
+            :prefix-icon="User"
+            placeholder="請輸入用戶名"
+          ></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input :prefix-icon="Lock" type="password" placeholder="請輸入密碼"></el-input>
+        <el-form-item prop="password">
+          <el-input
+            v-model="formModel.password"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="請輸入密碼"
+          ></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input :prefix-icon="Lock" type="password" placeholder="再次輸入密碼"></el-input>
+        <el-form-item prop="repassword">
+          <el-input
+            v-model="formModel.repassword"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="再次輸入密碼"
+          ></el-input>
         </el-form-item>
         <el-form-item>
           <el-button class="button" type="primary" auto-insert-space> 註冊 </el-button>
         </el-form-item>
         <el-form-item class="flex">
-          <el-link type="info" :underline="false" @click="isRegister = false"> ← 返回 </el-link>
+          <el-link type="info" :underline="false" @click="isRegister = false"> &lt; 返回 </el-link>
         </el-form-item>
       </el-form>
       <el-form ref="form" size="large" autocomplete="off" v-else>
         <!-- isRegister 為false，看到登錄相關表單  -->
         <el-form-item>
-          <h1>登錄</h1>
+          <h1>用戶登錄</h1>
         </el-form-item>
-        <el-form-item>
-          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
-        </el-form-item>
-        <el-form-item>
+        <el-form-item prop="username">
           <el-input
-            name="password"
+            v-model="formModel.username"
+            :prefix-icon="User"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="formModel.password"
             :prefix-icon="Lock"
             type="password"
             placeholder="请输入密码"
@@ -53,15 +94,15 @@ const isRegister = ref(true)
         </el-form-item>
         <el-form-item class="flex">
           <div class="flex">
-            <el-checkbox>记住我</el-checkbox>
-            <el-link type="primary" :underline="false">忘记密码？</el-link>
+            <el-checkbox>記住我</el-checkbox>
+            <el-link type="primary" :underline="false">忘記密碼？</el-link>
           </div>
         </el-form-item>
         <el-form-item>
-          <el-button class="button" type="primary" auto-insert-space>登录</el-button>
+          <el-button class="button" type="primary" auto-insert-space>登錄</el-button>
         </el-form-item>
         <el-form-item class="flex">
-          <el-link type="info" :underline="false" @click="isRegister = true"> 注册 → </el-link>
+          <el-link type="info" :underline="false" @click="isRegister = true"> 註冊 > </el-link>
         </el-form-item>
       </el-form>
     </el-col>
@@ -75,7 +116,6 @@ const isRegister = ref(true)
 
   .bg {
     background: url('@/assets/bg/bg.png') no-repeat center / cover;
-    // border-radius: 0 20px 20px 0;
   }
 
   .form {
@@ -90,12 +130,31 @@ const isRegister = ref(true)
 
     .button {
       width: 100%;
+      background-color: #a200ff;
+
+      &:hover {
+        background-color: #7000b1;
+      }
     }
 
     .flex {
       width: 100%;
       display: flex;
       justify-content: space-between;
+    }
+  }
+
+  h1 {
+    color: #bd30ff;
+  }
+
+  .el-link {
+    transition: all 0.3s;
+    color: #7a7a7a;
+
+    &:hover {
+      color: #cccccc;
+      transition: all 0.3s;
     }
   }
 }
